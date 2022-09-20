6daac762b7c6c40d02c835982f0ea0d8ca6d8898
Dhan Micheal Tamparong <111488321+su1kagg@users.noreply.github.com>
2022-09-20 08:57:25 +0800

    Create forloopYx10.java

---
+++ forloopYx10.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Input number to be multiplied: ");
+        int x = in.nextInt();
+
+        for(int i = 1; i <=10 ; i++){
+            System.out.println(x + "x" + i + "=" + x * i ) ;
+        }
+    }      
+}
