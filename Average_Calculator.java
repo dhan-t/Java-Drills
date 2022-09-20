eb890191aaf9b7e6ee541f35386656123f7efd31
Dhan Micheal Tamparong <111488321+su1kagg@users.noreply.github.com>
2022-09-20 10:01:00 +0800

    Create Average_Calculator.java

---
+++ Average_Calculator.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class App {
+    static Scanner in = new Scanner(System.in);
+    public static void main(String[] args) throws Exception {
+        System.out.println("Input 3 numbers to find out the average!");
+        System.out.println("Input first number: ");
+        int x = in.nextInt();
+
+        System.out.println("Input second number: ");
+        int y = in.nextInt();
+
+        System.out.println("Input third number: ");
+        int z = in.nextInt();
+
+        System.out.println("The average the three inputed number is: ");
+        System.out.println((x+y+z)/3);
+    }
+}      
