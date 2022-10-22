fc37f40215202b338aba12567af8165941e7d2be
Dhan Micheal Tamparong <111488321+su1kagg@users.noreply.github.com>
2022-10-22 21:15:45 +0800

    Update and rename evenoddcalc.java to even_odd_calculator.java

--- even_odd_calculator.java
+++ even_odd_calculator.java
@@ -1,16 +1,17 @@
 import java.util.Scanner;
 
-public class App {
+public class even_odd_calculator {
     static Scanner in = new Scanner(System.in);
     public static void main(String[] args) throws Exception {
        System.out.println("This program checks if the number is even or odd. \n insert the number you want to check:");
        int x = in.nextInt();
-
+       
        if (x%2== 0){
         System.out.println("Your number is " + x + "and it is even" );
        }
        else {
         System.out.println("Your number is " + x + "and it is odd");
+           
        }
     }
  }
