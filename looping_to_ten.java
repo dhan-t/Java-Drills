edb9e385d2a79f3bcb25a725c748055f872ea477
Dhan Micheal Tamparong <111488321+su1kagg@users.noreply.github.com>
2022-10-22 21:16:52 +0800

    Update and rename forloopYx10.java to looping_to_ten.java

--- looping_to_ten.java
+++ looping_to_ten.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class App {
+public class looping_to_ten {
     public static void main(String[] args) throws Exception {
         Scanner in = new Scanner(System.in);
         System.out.println("Input number to be multiplied: ");
